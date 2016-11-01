3d4a0c0ebbd02461c65b65439364a0350cd719d2
Matt Parish <mattparish@Matts-MacBook.local>
2016-11-01 09:15:36 +0000

    changed to nicer formatting of intent

--- index.js
+++ index.js
@@ -1,13 +1,76 @@
 'use strict';
 
+const Alexa = require('alexa-sdk');
+
 const TvGuide = require('./TvGuide');
 
-var tvGuide = new TvGuide('GB');
+var handlers = {
+  'ShowAirtimeIntent': showAirtimeIntent
+}
 
-tvGuide.getShow('humans')
-.then((showsArray) => tvGuide.findShowInMyCountry(showsArray))
-// the above is required for making sure we keep "this"
-.then(tvGuide.getNextEpisode)
-.spread(tvGuide.parseShowAndEpisodeDataToSpeech)
-.then((speakString) => console.log(speakString));
+exports.handler = function(event, context, callback){
+  var alexa = Alexa.handler(event, context);
+  alexa.registerHandlers(handlers);
+  alexa.execute();
+};
 
+
+
+
+
+
+function showAirtimeIntent(show) {
+  let self = this;
+  
+  let showToLookup;
+  
+  if (show) {
+    // useful for testing locally
+    showToLookup = show; 
+  } else {
+    showToLookup= this.event.request.intent.slots.Show.value;
+  }
+
+  let tvGuide = new TvGuide('GB');
+
+  // getShow -----------------------------------------------
+  let promise = tvGuide.getShow(showToLookup)
+  .catch(() => {
+    self.emit(':tell', `I couldn\'t find any shows by the name "${showToLookup}"`);
+    promise.cancel();
+  })
+
+  // findShowInMyCountry------------------------------------
+  .then((showsArray) => tvGuide.findShowInMyCountry(showsArray))
+  // the above is required for making sure we keep "this" inside the method.
+  .catch(() => {
+    self.emit(':tell', `I couldn\'t find any shows by the name "${showToLookup}" in your country`);
+    promise.cancel();
+  })
+
+  // getNextEpisode ----------------------------------------
+  .then(tvGuide.getNextEpisode)
+  .catch(() => {
+    self.emit(':tell', `I couldn\'t find any new episodes for "${showToLookup}"`);
+    promise.cancel();
+  })
+
+  // parseShowAndEpisodeDataToSpeech------------------------
+  .spread(tvGuide.parseShowAndEpisodeDataToSpeech)
+  .then((speakString) => { 
+    self.emit(':tell', speakString)
+  })
+  .catch((error) => {
+    self.emit(':tell', `Sorry, something went wrong looking up "${showToLookup}".  Please try again.`);
+    promise.cancel();
+  });
+}
+
+
+var fakeThis = {
+  emit: function(tellAsk, message) {
+    console.log(message);
+  }
+}
+
+showAirtimeIntent.call(fakeThis, 'strictly come dancing');
