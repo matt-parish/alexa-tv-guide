282a22c87575a65751a0b33a05639e593792ceca
Matt Parish <matt.j.parish@googlemail.com>
2016-11-04 19:29:58 +0000

    1

--- index.js
+++ index.js
@@ -14,33 +14,37 @@ exports.handler = function(event, context, callback){
   alexa.execute();
 };
 
-
+// make an intenthandlers file, and thne a file for each intent.
+// and a file for the core calls to the api.
+// that'll clean this file up massively.
 function showAirtimeIntent(show) {
-  let self = this;
-  
   let showToLookup;
+  let language;
   
-  if (show) {
-    // useful for testing locally
-    showToLookup = show; 
-  } else {
+  try {
+    // case where the call comes from Alexa.
+    // locale is sent to us in the format cc-LC
+    // (country code)-(LANGU?AGE CODE)
+    language = this.event.request.locale.split('-')[1]; 
     showToLookup= this.event.request.intent.slots.Show.value;
+    
+  } catch (e) {
+    // Otherwise we're in testing mode.
+    language = 'GB';
+    showToLookup = show; 
+    
   }
 
-  let tvGuide = new TvGuide('GB');
-
-
-  // ---------------------------------------------------
+  let tvGuide = new TvGuide(language);
   
-  // getShow -----------------------------------------------
-  let promise = tvGuide.getShow(showToLookup)
+  tvGuide.getShow(showToLookup)
   .then((showsArray) => tvGuide.findShowInMyCountry(showsArray))
   // the above is required for making sure we keep "this" inside the method.
   .then(tvGuide.getNextEpisode)
   .spread(tvGuide.parseShowAndEpisodeDataToSpeech)
   .catch((error) => tvGuide.errorHanding(error, showToLookup))
   .then((speakString) => { 
-    self.emit(':tell', speakString)
+    this.emit(':tell', speakString)
   })
   
 }
