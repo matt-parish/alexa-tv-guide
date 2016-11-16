6e59af74427ce3417304316401328e9d54a3ba47
Matt Parish <matt.j.parish@googlemail.com>
2016-11-16 18:34:00 +0000

    added a draft verficiation of alexa response.  This needs to be fully abstracted out.

--- handlers/ShowAirtimeIntent/index.js
+++ handlers/ShowAirtimeIntent/index.js
@@ -3,29 +3,32 @@
 const moment = require('moment');
 const Promise = require('bluebird');
 
+const getAlexaRequest = require('../../lib/helpers/getAlexaRequest');
+
 const TvGuide = require('../../lib/TvGuide');
 const constants = require('./../../lib/helpers/constants');
 
 function ShowAirtimeIntent() {
   let showToLookup;
   let language;
-  
 
   try {
-    showToLookup = this.event.request.intent.slots.Show.value;
-
-    // case where the call comes from Alexa.
-    // locale is sent to us in the format cc-LC
-    // (country code)-(LANGU?AGE CODE)
-    language = this.event.request.locale.split('-')[1];
-  } catch (e) {
-    // Otherwise default to GB
-    language = 'GB';
+    // Error handing to check that I'm being given everything I need by Alexa.
+    showToLookup = getAlexaRequest.slotValue(this.event.request, 'Show');
+    language = getAlexaRequest.localeInformation(this.event.request);
+  } catch (error) {
+    if (Object.keys(getAlexaRequest.throwables).indexOf(error.name) > -1 ) {
+      // If it's one of ours. use that message.
+      return this.emit(':tell', error.message());
+    } else {
+      // We should never get here, but just in case...
+      return this.emit(':tell', `Sorry, there was a problem looking up your show's start time.`);
+    }
   }
 
   let tvGuide = new TvGuide(language);
   
-  tvGuide.getShows(showToLookup)
+  return tvGuide.getShows(showToLookup)
   .then((showsArray) => tvGuide.findShowInMyCountry(showsArray, showToLookup))
   // the above is required for making sure we keep "this" inside the method.
   .then(tvGuide.getNextEpisode)
